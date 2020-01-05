f017c1c404a8cd3f2fc5bacd14dd7177b2c50ace
aafanasev <tafanasyev@gmail.com>
2020-01-05 23:21:40 +0700

    full card style

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -9,36 +9,43 @@ class PokerCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: const Color(0xFF616161),
-          borderRadius: const BorderRadius.all(const Radius.circular(10))),
+        color: const Color(0xFF616161),
+        borderRadius: const BorderRadius.all(const Radius.circular(10)),
+      ),
       padding: EdgeInsets.all(10),
       margin: EdgeInsets.all(4),
       child: Column(
         children: <Widget>[
-          Align(
-            alignment: Alignment.topLeft,
-            child: RotatedBox(
-              quarterTurns: 2,
-              child: Text(value,
-                  style: const TextStyle(color: const Color(0xFFFFFFFF))),
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: RotatedBox(
+                quarterTurns: 2,
+                child: Text(
+                  value,
+                  style: const TextStyle(fontSize: 24),
+                ),
+              ),
             ),
           ),
           Expanded(
-              child: Center(
-                  child: Text(value,
-                      style: const TextStyle(
-                        color: const Color(0xFFFFFFFF),
-                        fontSize: 10,
-                        decoration: null
-                        )
-                        )
-                        )
-                        ),
-          Align(
-            alignment: Alignment.topRight,
-            child: Text(value,
-                style: const TextStyle(color: const Color(0xFFFFFFFF))),
-          )
+            flex: 6,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: Text(value),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                value,
+                style: const TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
         ],
       ),
     );
