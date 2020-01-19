a6d9e8956901f7c723b4982386fe388fb79c6940
aafanasev <tafanasyev@gmail.com>
2020-01-19 23:33:27 +0700

    always keep on display on card screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/widgets.dart';
 import 'package:scrum_poker/screens/card_screen.dart';
 import 'package:scrum_poker/screens/home_screen.dart';
+import 'package:wakelock/wakelock.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WidgetsApp(
-      color: Color(0xFF000000),
+      color: const Color(0xFF000000),
       onGenerateRoute: (settings) => PageRouteBuilder(
         settings: settings,
         pageBuilder: (context, animation, secondaryAnimation) {
+          Wakelock.toggle(on: settings.name == '/card');
+
           switch (settings.name) {
             case '/':
               return HomeScreen();
